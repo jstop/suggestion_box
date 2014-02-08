5896708c60ab4c138312bf395c2f52fdd2c27f33
jstop <jstein@clypd.com>
2014-02-08 16:24:50 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuggestionBox::Application.config.secret_token = '2819655baca0ade7372bf0d0af7f3b2deeb237f423d97c0ef65809e781f44db677dd4da2305e91e75f4073e212082c0f380f64d3ee4cede0d27254542a757622'
